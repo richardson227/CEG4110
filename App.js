d0d62008fc884592c2adcccf301100717ee0bb70
Zach Richardson <zrichardson.218@google.com>
2018-09-09 20:29:34 -0400

    Integrated part 1 of hw

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Text, View, StyleSheet, TextInput, Button, label } from 'react-native'; // Version can be specified in package.json
 
+/**
+ * Zach Richardson
+ * Ceg 4110
+ */
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      textOutput: 'Tap to change color',
+      color: '#000000',
+    };
+  }
+
+  changeColor = () => {
+    var newColor =
+      '#' +
+      Math.random()
+        .toString(16)
+        .substr(-6);
+    this.setState({color: newColor });
+  };
+
+  rgbValues = () => {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
+      this.state.color
+    );
+    return result
+      ? {
+          r: parseInt(result[1], 16),
+          g: parseInt(result[2], 16),
+          b: parseInt(result[3], 16),
+        }
+      : null;
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <TextInput
+        style={{ color: this.state.color, fontSize: 30}}
+        onChangeText={(textOutput) => {
+          this.setState({textOutput})}}
+        value={this.state.textOutput}
+      />
+        <Text
+          style={{ fontSize: 20, marginBottom: 5 }}>
+          {this.state.color}
+        </Text>
+        <Text style={{ fontSize: 15, marginBottom: 20 }}>
+          {' '}
+          R:{this.rgbValues().r} G:{this.rgbValues().g} B:{this.rgbValues().b}{' '}
+        </Text>
+        <Button onPress={this.changeColor} title="Change Color" />
       </View>
     );
   }
@@ -16,8 +63,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
   },
 });
